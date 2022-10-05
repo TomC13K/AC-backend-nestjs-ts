54a34af801629657daae8dba5164dc6f59af68a9
“Tom” <email>
2022-10-05 12:46:08 +0100

    created config for dev and prod env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { CalendarService } from './calendar/calendar.service';
 import { CalendarController } from './calendar/calendar.controller';
 import { DashboardController } from './dashboard/dashboard.controller';
 import { DashboardService } from './dashboard/dashboard.service';
+import { ConfigModule } from '@nestjs/config';
+import { getEnvPath } from './common/helper/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true })],
   controllers: [AppController, CalendarController, DashboardController],
   providers: [AppService, CalendarService, DashboardService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+
+  @Inject(ConfigService)
+  public config: ConfigService;
+
   getHello(): string {
     return 'Hello World!';
   }
