560f5070b5a59491b56842de6ff5441b97174909
“Tom” <email>
2022-10-07 09:50:07 +0100

    customer db create

---
+++ src/customers/dtos/CreateCustomer.dto.ts
@@ -0,0 +1,13 @@
+export class CreateCustomerDto {
+    customerID: number;
+    firstName: string;
+    lastName: string;
+    businessName: string;
+    phone: string;
+    emailName: string;
+    ico: string;
+    dic: string;
+    street: string;
+    town: string;
+    psc: string;
+}
